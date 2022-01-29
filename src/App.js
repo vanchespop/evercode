624a4abb01e856993e5cc4da0e45bd04ad3cdafc
Иван Попов <vanchespop@yandex.ru>
2022-01-29 15:36:54 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="container">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, dolorum.
+            </div>
+        </div>
+    );
+}
+
+export default App;
